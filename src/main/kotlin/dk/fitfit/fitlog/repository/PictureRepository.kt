32417e32c66233dc99102f6ec060339983b88309
sj <sebastianthegreatful@gmail.com>
2020-04-20 18:20:34 +0700

    Remove unused import

--- src/main/kotlin/dk/fitfit/fitlog/repository/PictureRepository.kt
+++ src/main/kotlin/dk/fitfit/fitlog/repository/PictureRepository.kt
@@ -1,7 +1,6 @@
 package dk.fitfit.fitlog.repository
 
 import dk.fitfit.fitlog.domain.Picture
-import dk.fitfit.fitlog.domain.Video
 import io.micronaut.data.annotation.Repository
 import io.micronaut.data.repository.CrudRepository
 
