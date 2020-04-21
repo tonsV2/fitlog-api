1ffa4a639f59b713f45c9d5e6433e489cffcd165
sj <sebastianthegreatful@gmail.com>
2020-04-21 09:46:14 +0700

    Add created and updated properties to User

--- src/main/kotlin/dk/fitfit/fitlog/domain/User.kt
+++ src/main/kotlin/dk/fitfit/fitlog/domain/User.kt
@@ -1,5 +1,7 @@
 package dk.fitfit.fitlog.domain
 
+import io.micronaut.data.annotation.DateCreated
+import io.micronaut.data.annotation.DateUpdated
 import java.time.LocalDateTime
 import javax.persistence.*
 
@@ -8,11 +10,14 @@ import javax.persistence.*
 class User(
         @Column(nullable = false, unique = true) val email: String,
         val name: String,
-        val created: LocalDateTime = LocalDateTime.now(),
         @ManyToMany(fetch = FetchType.EAGER)
         @JoinTable(name = "users_roles",
                 joinColumns = [JoinColumn(name = "user_id", referencedColumnName = "id")],
                 inverseJoinColumns = [JoinColumn(name = "role_id", referencedColumnName = "id")])
         val roles: MutableList<Role> = mutableListOf(),
+        @DateCreated
+        var created: LocalDateTime = LocalDateTime.MIN,
+        @DateUpdated
+        var updated: LocalDateTime = LocalDateTime.MIN,
         @Id @GeneratedValue(strategy = GenerationType.SEQUENCE) val id: Long = 0
 )
